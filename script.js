e9df9fa3fcd6dbabeb7aeefea55d875c352249dc
jblaeser <44827644+jblaeser@users.noreply.github.com>
2020-10-27 14:18:13 -0500

    homepage instagram feed

---
+++ script.js
@@ -0,0 +1,6 @@
+// curator-feed-homepagefeatured-layout
+(function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/3a91be96-3801-4797-80f9-0a9d5b0db399.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
