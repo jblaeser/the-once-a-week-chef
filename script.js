ba4cab4a9841efd9d5a9e7371656832016d3cdfb
jblaeser <44827644+jblaeser@users.noreply.github.com>
2020-10-30 16:52:35 -0500

    php form test

--- script.js
+++ script.js
@@ -23,3 +23,7 @@ var flkty = new Flickity( elem, {
     resize: true,
     });
         
+    var frmvalidator = new Validator(“contactform”); 
+    frmvalidator.addValidation(“name”,”req”,”Please provide your name”); 
+    frmvalidator.addValidation(“email”,”req”,”Please provide your email”); 
+    frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”);
